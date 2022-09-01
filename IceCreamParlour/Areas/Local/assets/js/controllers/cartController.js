d471f1522e10b42a5d8125f9e8f2a47d97071004
Minhthai0922 <nguyenminhthai22092000@gmail.com>
2022-09-01 21:30:55 +0700

    Thainm

--- IceCreamParlour/Areas/Local/assets/js/controllers/cartController.js
+++ IceCreamParlour/Areas/Local/assets/js/controllers/cartController.js
@@ -58,7 +58,7 @@
                 type: 'POST',
                 success: function (res) {
                     if (res.status == true) {
-                        window.location.href = "/gio-hang";
+                        window.location.href = "/Cart";
                     }
                 }
             })
